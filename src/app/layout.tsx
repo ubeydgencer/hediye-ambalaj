17f94dc2b7c97d869ce379e41a84e87f70f44f6a
UBEYD GENCER <mubeyd@gmail.com>
2026-05-11 16:54:59 +0300

    feat: add Google Analytics G-VGTWR6R1E0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,6 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" className="h-full">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VGTWR6R1E0"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VGTWR6R1E0');
+        `}</Script>
+      </head>
       <body className="min-h-full flex flex-col antialiased">{children}</body>
     </html>
   );
